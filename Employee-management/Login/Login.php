6d712782284dbff35be0902f45a949caa29decdb
name-aniket <60316529+name-aniket@users.noreply.github.com>
2020-03-29 14:46:57 +0530

    Add files via upload

--- Employee-management/Login/Login.php
+++ Employee-management/Login/Login.php
@@ -1,46 +1,110 @@
 <?php
-	class Login {
-        public static $dbConnection = null;
-        const Relation = 'Login';
+class Login
+{
+    public static $dbConnection = null;
+    const Relation = 'Login';
 
-        /**
-         * 
-         * 
-         */
-        public static function factoryCreateConnection() {
-            include_once '../../IWP/config/core.php';
-            include_once '../../IWP/config/database.php';
+    /**
+     * 
+     * 
+     */
+    public static function factoryCreateConnection()
+    {
+        include_once '../../IWP/config/core.php';
+        include_once '../../IWP/config/database.php';
 
-            $dbObject = new Database($dbconfig['username'], $dbconfig['password']);
-            
-            if (Login::$dbConnection === null){
-                Login::$dbConnection = $dbObject->getConnection();
-            }
-        }
-        
-        /**
-         * 
-         * 
-         */
-        public static function authenticate() {
-            /**
-             * Create connection
-             */
-            Login::factoryCreateConnection();
+        $dbObject = new Database($dbconfig['username'], $dbconfig['password']);
 
-            /**
-             * Validate input
-             */
-            $username = htmlspecialchars(stripslashes($_POST['username']));
-            $password = htmlspecialchars(stripslashes($_POST['passwd']));
-
-            if (!filter_var($username, FILTER_VALIDATE_EMAIL)) { 
-                return false;
-            }
-
-            
-            return true;
+        if (Login::$dbConnection === null) {
+            Login::$dbConnection = $dbObject->getConnection();
         }
     }
 
-?>
+    /**
+     * 
+     * 
+     */
+    public static function authenticate()
+    {
+        /**
+         * Create connection
+         */
+        Login::factoryCreateConnection();
+
+        /**
+         * Validate input
+         */
+        $username = htmlspecialchars(stripslashes($_POST['username']));
+        $password = htmlspecialchars(stripslashes($_POST['passwd']));
+
+        /**
+         * 
+         */
+        if (!filter_var($username, FILTER_VALIDATE_EMAIL)) {
+            return array(
+                'Message' => "Invalid email",
+                'Status' => false
+            );
+        }
+
+        /**
+         *  Get password form the database.
+         *  
+         */
+        $stmt = Login::$dbConnection->prepare(
+            'SELECT password, user_type, emp_id, emp_profilePhoto, emp_fname, emp_lname FROM Login L, Employee E WHERE  L.email = ? AND E.emp_email = L.email'
+        );
+
+        /**
+         * Execute query
+         */
+        $flag = $stmt->execute(array(
+            $username
+        ));
+
+        /**
+         * If password query doesn't run successfully
+         */
+        if (!$flag) {
+            return array(
+                'Message' => "Error",
+                'Status' => false
+            );
+        }
+
+        /**
+         * Match the password
+         */
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($password == $row['password']) {
+            if ($row['user_type'] == 'HR') {
+                $location = 'HR';
+            } else if ($row['user_type'] == 'EMP') {
+                $location = 'EMP';
+            }
+
+            session_start();
+
+            $_SESSION['user_detail'] = array(
+                'emp_id' => $row['emp_id'],
+                'emp_profilePhoto' => $row['emp_profilePhoto'],
+                'emp_fname' => $row['emp_fname'],
+                'emp_lname' => $row['emp_lname'],
+                'email' => $username,
+                'password' => $password
+            );
+
+            return array(
+                'Message' => $location,
+                'Status' => true
+            );
+
+        } else {
+            return array(
+                'Message' => "Invalid Password or Username",
+                'Status' => false
+            );
+        }
+    }
+}
--- Employee-management/Login/check_login.php
+++ Employee-management/Login/check_login.php
@@ -4,6 +4,7 @@
   header("Access-Control-Allow-Methods: POST");
   header("Access-Control-Max-Age: 3600");
   header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+  
   include_once './Login.php';
   
   echo json_encode(Login::authenticate());
